c70b169bfc690842f16a56499625663d63ba4e5a
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-08-29 13:53:36 +0700

    feat: uuid and event response pub

--- teperunner-worker/src/main.rs
+++ teperunner-worker/src/main.rs
@@ -1,4 +1,6 @@
 use dotenvy::dotenv;
+use redis::Commands;
+use uuid::Uuid;
 
 mod conn;
 mod schema;
@@ -8,9 +10,11 @@ async fn main() -> redis::RedisResult<()> {
     // Load Env
     dotenv().ok();
 
-    let mut conn = conn::get_redis_connection().await?;
-    let mut pubsub = conn.as_pubsub();
+    let mut sub_conn = conn::get_redis_connection().await?;
+    let mut pubsub = sub_conn.as_pubsub();
+
     let subs_channel = "runner_channel";
+    let response_channel = "response_channel";
 
     pubsub.subscribe(subs_channel)?;
     println!("Listening for commands on '{}'...", subs_channel);
@@ -20,11 +24,35 @@ async fn main() -> redis::RedisResult<()> {
         let payload: String = msg.get_payload()?;
         println!("Received message: {:?}", msg);
 
-        let event_data: schema::EventData = serde_json::from_str(&payload)
-            .map_err(|e| redis::RedisError::from((redis::ErrorKind::TypeError, "JSON deserialization error", e.to_string())))?;
+        let event_data: schema::EventData = serde_json::from_str(&payload).map_err(|e| {
+            redis::RedisError::from((
+                redis::ErrorKind::TypeError,
+                "JSON deserialization error",
+                e.to_string(),
+            ))
+        })?;
 
         // TODO: Execute Jar File
-    }
 
-    Ok(())
+        let event_response = schema::EventResponse {
+            content: format!("Executed command: {}", event_data.jar_path),
+            id: Uuid::new_v4().to_string(),
+            status: "success".to_string(),
+            is_error: false,
+        };
+
+        let message = serde_json::to_string(&event_response).map_err(|e| {
+            redis::RedisError::from((
+                redis::ErrorKind::TypeError,
+                "JSON serialization error",
+                e.to_string(),
+            ))
+        })?;
+
+        println!("Publishing response: {}", message);
+
+        // Use a separate connection for publishing
+        let mut pub_conn = conn::get_redis_connection().await?;
+        pub_conn.publish::<_, _, ()>(response_channel, message)?;
+    }
 }
